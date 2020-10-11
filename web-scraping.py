3e103a01c91c95625c5ae3542c5d5e8d73afda39
gumball7112 <47183131+gumball7112@users.noreply.github.com>
2020-10-11 09:47:42 +0700

    Add files via upload

---
+++ web-scraping.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+'''scraping kênh 14'''
+
+search = input('Enter your website:')  # take in search request
+# params = {'q': search}  # assign q
+r = requests.get(f'https://kenh14.vn/{search}.html')  # access web
+soup = BeautifulSoup(r.text, 'html.parser')  # parse through the source
+result = soup.find('li', {'class': 'knswli need-get-value-facebook clearfix'})
+
+for result in soup.find_all('li', {'class': 'knswli need-get-value-facebook clearfix'}):
+
+    headline = result.h3.a.text
+    print(headline)
+
+    intro = result.find('span', {'class': 'knswli-sapo'})
+    if intro is None:
+        print('This article has no intro')
+    else:
+        print(intro.text)
+
+    link = result.find('a', {'class': 'kscliw-ava'})['href']
+    print('https://kenh14.vn' + link + '\n')
+
+# print(result.prettify())
